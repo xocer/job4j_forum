52503f2d1f55f2f2d8d90003eb51d62df54ebc03
xocer <vitamin9292@mail.ru>
2021-10-27 21:09:24 +0300

    add repositories

---
+++ src/main/java/ru/job4j/forum/store/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.job4j.forum.store;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.job4j.forum.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserByUsername(String username);
+}
