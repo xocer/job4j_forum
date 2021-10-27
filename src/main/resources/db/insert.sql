7475bd5cc0b246cd35cd6718b688b0361a6b0c2c
xocer <vitamin9292@mail.ru>
2021-10-27 21:06:12 +0300

    update db schema

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -3,5 +3,9 @@ values ('ROLE_USER');
 insert into authorities (authority)
 values ('ROLE_ADMIN');
 
-insert into posts (name) values ('О чем этот форум?');
-insert into posts (name) values ('Правила форума.');
+insert into users (username, password, enabled, authority_id)
+values ('test', '$2a$10$XMt2m4/AAhSNolNL4uxE2eYJQPHkiYifAvpSkiq3hK4K/.RR6V3UK', true,
+        (select id from authorities where authority = 'ROLE_ADMIN'));
+
+insert into posts (name, user_id) values ('О чем этот форум?', 1);
+insert into posts (name, user_id) values ('Правила форума.', 1);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE if not exists users
     username     VARCHAR(50)  NOT NULL unique,
     password     VARCHAR(200) NOT NULL,
     enabled      boolean default true,
-    authority_id int          not null references authorities (id)
+    authority_id int not null references authorities (id)
 );
 
 -- drop table if exists topics cascade ;
@@ -21,5 +21,6 @@ create table if not exists posts
     id        serial primary key,
     name      varchar(200),
     description varchar(200),
-    created     timestamp without time zone not null default now()
+    created     timestamp without time zone not null default now(),
+    user_id int references users (id)
 );
